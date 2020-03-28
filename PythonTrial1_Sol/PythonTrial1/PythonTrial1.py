39fe390b3b6d78379b29cd1dd66892639f9ccaec
Luis Ruiz Matallana <LRUIZMAT@ford.com>
2020-03-28 21:36:20 +0100

    training

--- PythonTrial1_Sol/PythonTrial1/PythonTrial1.py
+++ PythonTrial1_Sol/PythonTrial1/PythonTrial1.py
@@ -1,6 +1,12 @@
 import numpy as np
+
 list1 = [[1,3,1],[2,1,2],[3,2,3]]
 np_array1 = np.array(list1)
 np_array2 = np_array1*3
 np_result = np_array2 - np_array1
 print(np_result)
+norm_array = np.random.normal(0,0.66,1000)
+non_norm_array = np.random
+
+import matplotlib as plt
+
---
+++ PythonTrial1_Sol/PythonTrial1/PythonTrial2_randomnormal.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+norm_array = np.random.normal(0,0.66,1000)
+non_norm_array = np.random
+
+import matplotlib as plt
