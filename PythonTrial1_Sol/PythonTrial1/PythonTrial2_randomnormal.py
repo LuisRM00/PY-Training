ae4a42ea31fd9a1a6dfbfd261dc1e510c87c8e99
Luis Ruiz Matallana <LRUIZMAT@ford.com>
2020-03-28 21:42:18 +0100

    training

--- PythonTrial1_Sol/PythonTrial1/PythonTrial2_randomnormal.py
+++ PythonTrial1_Sol/PythonTrial1/PythonTrial2_randomnormal.py
@@ -3,4 +3,4 @@ import numpy as np
 norm_array = np.random.normal(0,0.66,1000)
 non_norm_array = np.random
 
-import matplotlib as plt
+import matplotlib.pyplot as plt
--- PythonTrial1_Sol/PythonTrial1/PythonTrial3_matplotlib.py
+++ PythonTrial1_Sol/PythonTrial1/PythonTrial3_matplotlib.py
@@ -1,1 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+year = [1950,1970,1990,2010]
+pop = [2.159,3.692,5.263,6.972]
+
+plt.plot(year,pop)
+plt.show()
